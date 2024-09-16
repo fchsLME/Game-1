ffc3b543a411aadacdfa6cd22c3d13d0d61e64dc
fchsLME <lestes0@ftstudent.org>
2024-09-16 13:41:25 -0400

    Changes

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # Platformer Project
-insert description here
+2D obstacle course game
 
 
 ## Team Members
 ### fchsLemke - Mr. Lemke
-set up repo with readme, git config, and empty project files
+fchsLME-Logan E
 ### [Now add each collaborator the same way]
-* [and their contributions]
-* [using this format if you want bullets]
+everything
 
 
 ## Reflections
 
-Your write up will go here
+I made a starting screen and button.Another change I made was the ability to jump on enemies and kill them.The final change I made was a double jump.I thought it was an overall fun project.The most difficult part of it was making enemies die by being jumped on but other than that it wasnt that bad.
---
+++ Scripts/player.gd
@@ -0,0 +1,53 @@
+extends CharacterBody2D
+class_name Player
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+var jump_max = 2
+var jump_count = 0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if is_on_floor() and jump_count != 0:
+		jump_count = 0
+
+	if jump_count < jump_max:
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_VELOCITY
+			jump_count +=1
+
+# Get the input direction: -1, 0, 1
+	var direction = Input.get_axis("move_left", "move_right")
+
+	#Flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction <0:
+		animated_sprite.flip_h = true
+
+		#Play animations
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
+		else:
+			animated_sprite.play("jump")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+	
